2f807dd9e591f67edced69ecba6db19dfed0fede
Vivek Prakash <vivek@world.(none)>
2013-10-13 18:07:25 +0530

    First commmit

---
+++ workshop/views.py
@@ -0,0 +1,10 @@
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+
+def home(request):
+    template = 'home.html'
+    users = User.objects.filter(is_superuser=False)
+    ctx = { 'users': users }
+    ctx = RequestContext(request, ctx)
+    return render_to_response(template, ctx)
